a17173904a3cc54f945f33623f9f865df41f8d7b
vastyoung <406433192@qq.com>
2021-12-12 14:18:11 +0800

    将缩进转化为空格

--- life_game.py
+++ life_game.py
@@ -2,20 +2,20 @@ from pprint import pprint
 
 
 def print_frame(frame):
-	for row in frame:
-		print(" ".join(row))
+    for row in frame:
+        print(" ".join(row))
 
 
 def main():
-	frame = []
-	for _ in range(10):
-		row_list = []
-		for _ in range(10) :
-			row_list.append("0")
-		frame.append(row_list)
+    frame = []
+    for _ in range(10):
+        row_list = []
+        for _ in range(10) :
+            row_list.append("0")
+        frame.append(row_list)
 
-	print_frame(frame)
+    print_frame(frame)
 
 
 if __name__ =="__main__":
-	main()
+    main()
