08b621cab5245b691fa7e89a5617591f80489f3a
vastyoung <406433192@qq.com>
2021-12-12 14:03:05 +0800

    生命游戏初始化提交

---
+++ life_game.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+
+
+def print_frame(frame):
+	for row in frame:
+		print(" ".join(row))
+
+
+def main():
+	frame = []
+	for _ in range(10):
+		row_list = []
+		for _ in range(10) :
+			row_list.append("0")
+		frame.append(row_list)
+
+	print_frame(frame)
+
+
+if __name__ =="__main__":
+	main()
