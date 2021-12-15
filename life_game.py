cbd899c76bb9da0cb1cfcdf0ee3026b96875904d
vastyoung <406433192@qq.com>
2021-12-16 01:01:26 +0800

    帧生成框架

--- life_game.py
+++ life_game.py
@@ -1,21 +1,40 @@
-from pprint import pprint
+import sys
+from time import sleep
+
+
+def init_frame(row, col):
+    frame = []
+    for _ in range(row):
+        frame.append(["0" for _ in range(col)])
+    return frame
+
+
+def next_frame(cur_frame):
+    new_frame = []
+    for row in cur_frame:
+        new_frame.append([str(int(i) + 1) for i in row])
+    return new_frame
 
 
 def print_frame(frame):
+    row_num = len(frame)
+    print("\033[{}A".format(row_num + 1))
     for row in frame:
         print(" ".join(row))
 
 
 def main():
-    frame = []
-    for _ in range(10):
-        row_list = []
-        for _ in range(10) :
-            row_list.append("0")
-        frame.append(row_list)
+    if len(sys.argv) != 3:
+        print("usage: {} <row> <col>, such as: {} 10 8".format(sys.argv[0], sys.argv[0]))
+        sys.exit(1)
 
-    print_frame(frame)
+    row, col = [int(arg) for arg in sys.argv[1:]]
+    frame = init_frame(row, col)
+    while frame:
+        print_frame(frame)
+        frame = next_frame(frame)
+        sleep(1)
 
 
-if __name__ =="__main__":
+if __name__ == '__main__':
     main()
